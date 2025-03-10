48d1ac40fcceb64e589e06f8693f8325a8d49fcf
Yunus Emre ASLAN <40874281+Dubium0@users.noreply.github.com>
2025-03-10 21:31:50 +0300

    sheesh

--- vendor/windows/fmt/run-msbuild.bat
+++
@@ -1,3 +0,0 @@
-
-    
-    C:/Program Files/Microsoft Visual Studio/2022/Preview/MSBuild/Current/Bin/amd64/MSBuild.exe -p:FrameworkPathOverride="C:\Program Files\Reference Assemblies\Microsoft\Framework;.NETFramework\v4.0" %*
